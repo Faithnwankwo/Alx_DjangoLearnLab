cec0afeaf30c9436bdfe769a592b6424bb76e78e
Faith Nwankwo <nkediya1948@gmail.com>
2025-08-30 21:37:57 -0600

    Register bookshelf.Book in admin with list_display, filters, search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # columns in the list page
+    list_display = ("title", "author", "publication_year")
+    # right-hand filters
+    list_filter = ("author", "publication_year")
+    # search box (case-insensitive)
+    search_fields = ("title", "author")
+    # optional niceties
+    ordering = ("title",)
+    list_per_page = 25
+
